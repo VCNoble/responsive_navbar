1676bcb5a661c9236d396ea08e5d8d40070ec80c
VienCN <noble.vien@gmail.com>
2022-10-04 20:05:00 +1300

    initial commmit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar/>
+      
+    </React.Fragment>
+
+  );
+}
+
+export default App;
